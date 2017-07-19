96f25699b897e5e660d668439e92d400ad8167ad
chen3 <chen3@qiditu.cn>
2017-07-19 10:01:05 +0800

    create ui
    use MaterialDesignInXamlToolkit

--- NightModeMaterialDesignUI/MainWindow.xaml.cs
+++ NightModeMaterialDesignUI/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NightModeMaterialDesignUI
 {
@@ -23,6 +12,12 @@ namespace NightModeMaterialDesignUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private Lazy<PaletteHelper> paletteHelper = new Lazy<PaletteHelper>();
+        private void NightTheme_Click(object sender, RoutedEventArgs e)
+        {
+            paletteHelper.Value.SetLightDark(NightTheme.IsChecked.GetValueOrDefault());
         }
     }
 }
